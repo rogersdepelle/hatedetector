df42e9d335b2a18999bf6e85fcfc90f05a70b76d
rogersdepelle <rogersdepelle@gmail.com>
2017-01-12 15:30:50 -0300

    Export dataset

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stock_exchange_game project.
+WSGI config for hatedetector project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stock_exchange_game.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hatedetector.settings")
 
 application = get_wsgi_application()
